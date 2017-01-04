248c5d7a84ff9492676e73dad8baa40af1360455
cuis509 <hncuis@qq.com>
2017-01-04 13:39:14 +0800

    Delete p1_6_check_getchar.c

--- p1_6_check_getchar.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-// c = (5/9) * (f-32)
-// f = ((9*c)/5) + 32
-int main () {
-    printf("Value if (getchar() != EOF) is %d\n", (getchar() != EOF));
-    getchar();
-    return 0;
-}
